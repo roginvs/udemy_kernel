0dcf5767a37a5b00b857b984e0cad2854d2875fe
Nibblebits <dansblackberry2@hotmail.co.uk>
2020-10-14 13:57:36 +0100

    Lecture 51 - Implementing The FAT16 Resolver Function

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -5,7 +5,6 @@
 
 typedef unsigned int PEACHOS_DISK_TYPE;
 
-
 // Represents a real physical hard disk
 #define PEACHOS_DISK_TYPE_REAL 0
 
@@ -13,12 +12,18 @@ struct disk
 {
     PEACHOS_DISK_TYPE type;
     int sector_size;
-    struct filesystem* filesystem;
+
+    // The id of the disk
+    int id;
+
+    struct filesystem *filesystem;
+
+    // The private data of our filesystem
+    void *fs_private;
 };
 
 void disk_search_and_init();
-struct disk* disk_get(int index);
-int disk_read_block(struct disk* idisk, unsigned int lba, int total, void* buf);
+struct disk *disk_get(int index);
+int disk_read_block(struct disk *idisk, unsigned int lba, int total, void *buf);
 
-
-#endif 
+#endif
