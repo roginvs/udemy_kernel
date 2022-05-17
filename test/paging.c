99e801cb08182db632efdee23d7760ba816e9ef5
Vasilii Rogin <vasya@rogin.ru>
2022-05-17 19:40:59 +0300

    Add tests for paging

--- test/paging.c
+++ test/paging.c
@@ -26,8 +26,31 @@ int test_zero_allocations()
 
 int test_paging_new_4gb()
 {
-    struct paging_4gb_chunk *kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
-    paging_free_4gb(kernel_chunk);
+    uint8_t test_flags_array[] = {0, PAGING_IS_PRESENT, PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL};
+    for (int i = 0; i < sizeof(test_flags_array) / sizeof(uint8_t); i++)
+    {
+        uint8_t test_flags = test_flags_array[i];
+
+        struct paging_4gb_chunk *chunk_4gb = paging_new_4gb(test_flags);
+        uint32_t *directory_table = chunk_4gb->directory_entry;
+
+        // First table, 1024 items
+        for (int table_index = 0; table_index < 1024; table_index++)
+        {
+            uint32_t *table = (uint32_t *)(directory_table[table_index] & 0xFFFFF000);
+
+            for (int table_table_index = 0; table_table_index < 1024; table_table_index++)
+            {
+                uint32_t table_address = 1024 * 4096 * table_index + table_table_index * 4096;
+                CU_ASSERT_EQUAL(table[table_table_index] & 0xFFFFF000, table_address); // Points to zero address in memory
+                CU_ASSERT_EQUAL(table[table_table_index] & 0x00000FFF, test_flags);
+            }
+        }
+
+        paging_free_4gb(chunk_4gb);
+
+        CU_ASSERT_EQUAL(mock_allocs_count, 0);
+    }
 }
 void add_paging_suite()
 {
