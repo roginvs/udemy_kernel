ce12e0587e7a31642b3fafa25abf58cf87d1cbb5
Vasilii Rogin <vasya@rogin.ru>
2022-05-26 13:51:20 +0300

    Add writing to output buf directly in the blank program

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -6,5 +6,21 @@ global _start
 
 _start:
 
+
+print_message:
+    mov esi, msg1
+    mov ecx, [0xB8000]
+    .loop:
+      lodsb ; Loads the byte from the SI address into the AL register and inrement SI
+      cmp al, 0 ; Compare the byte to 0
+      je .done
+      mov [ecx], al
+      add ecx, 2
+    jmp .loop
+   .done:
+      
+
 label:
     jmp label
+
+msg1 db "User program started",0
