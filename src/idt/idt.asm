4dfb844c3f07b3afc6a5083627adb5b868ce2d3d
Vasilii Rogin <vasya@rogin.ru>
2022-05-28 11:51:31 +0300

    Adding Lecture 74

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -27,20 +27,55 @@ idt_load:
 
 
 global int21h
-int21h:
-    cli
+int21h:    
     pushad ; push all general-purpose registers    
     call int21h_handler
     popad
-    sti
     iret
 
 extern no_interrupt_handler
 global no_interrupt
 no_interrupt:
-    cli
     pushad ; push all general-purpose registers    
     call no_interrupt_handler
     popad
-    sti
     iret
+
+
+extern isr80h_handler
+global isr80h_wrapper
+
+; No need to have "cli" instruction because of flags on interrupt handlers
+; idt_descriptor->type_attr, lower 4 bits = "32 bit interrupt gate"
+; and gate clears IF flag (original flags are on the stack and restored via iret)
+
+isr80h_wrapper:
+    ; INTERRUPT FRAME START
+    ; ALREADY PUSHED TO US BY THE PROCESSOR UPON ENTRY TO THIS INTERRUPT
+    ; uint32_t ip
+    ; uint32_t cs;
+    ; uint32_t flags
+    ; uint32_t sp;
+    ; uint32_t ss;
+    ; Pushes the general purpose registers to the stack
+    pushad
+    
+    ; INTERRUPT FRAME END
+
+    ; Push the stack pointer so that we are pointing to the interrupt frame
+    push esp
+
+    ; EAX holds our command lets push it to the stack for isr80h_handler
+    push eax
+    call isr80h_handler
+    mov dword[tmp_res], eax
+    add esp, 8
+
+    ; Restore general purpose registers for user land
+    popad
+    mov eax, [tmp_res]
+    iretd
+
+section .data
+; Inside here is stored the return result from isr80h_handler
+tmp_res: dd 0 
