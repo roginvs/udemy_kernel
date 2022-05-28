c04bddae4b7e983e681b5479c8365355560670ba
Vasilii Rogin <vasya@rogin.ru>
2022-05-28 12:12:56 +0300

    Adding Lecture 74

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -7,43 +7,47 @@
 struct idt_desc idt_descriptors[PEACHOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
-
-extern void idt_load(struct idtr_desc * ptr);
+extern void idt_load(struct idtr_desc *ptr);
 
 extern void int21h();
 extern void no_interrupt();
 
-void int21h_handler() {
+void int21h_handler()
+{
     print("Keyboard pressed");
     outb(0x20, 0x20); // Send an acknowledgement to the PIC
 }
-void idt_zero(){
+void idt_zero()
+{
     print("Device by zero error");
 }
 
-void no_interrupt_handler() {
+void no_interrupt_handler()
+{
     outb(0x20, 0x20); // Send an acknowledgement to the PIC
 }
 
-void idt_set(int interrupt_no, void * address) {
-    struct idt_desc * idt_descriptor = &idt_descriptors[interrupt_no];
+void idt_set(int interrupt_no, void *address)
+{
+    struct idt_desc *idt_descriptor = &idt_descriptors[interrupt_no];
 
-
-    idt_descriptor->offset_low = (uint16_t) ((uint32_t) address & 0x0000FFFF);
+    idt_descriptor->offset_low = (uint16_t)((uint32_t)address & 0x0000FFFF);
     idt_descriptor->selector = KERNEL_CODE_SELECTOR;
     idt_descriptor->zero = 0;
     // lower 1110 is 32 bit interrupt gate
     // higher 1110 is Present - Descriptor Privilege Level - Storage Segment
     idt_descriptor->type_attr = 0b11101110;
-    idt_descriptor->offset_high = (uint16_t) (((uint32_t) address & 0xFFFF0000) >> 16);
+    idt_descriptor->offset_high = (uint16_t)(((uint32_t)address & 0xFFFF0000) >> 16);
 }
 
-void idt_init() {
+void idt_init()
+{
     memset(idt_descriptors, 0, sizeof(idt_descriptors));
     idtr_descriptor.limit = sizeof(idt_descriptors) - 1;
     idtr_descriptor.base = (uint32_t)idt_descriptors;
 
-    for (int i = 0; i < PEACHOS_TOTAL_INTERRUPTS; i++) {
+    for (int i = 0; i < PEACHOS_TOTAL_INTERRUPTS; i++)
+    {
         idt_set(i, no_interrupt);
     }
     idt_set(0, idt_zero);
@@ -52,4 +56,16 @@ void idt_init() {
     idt_load(&idtr_descriptor);
 }
 
+void isr80h_handle_command(int command, struct interrupt_frame *frame)
+{
+}
 
+void *isr80h_handler(int command, struct interrupt_frame *frame)
+{
+    void *res = 0;
+    kernel_page();
+    task_current_save_state(frame);
+    res = isr80h_handle_command(command, frame);
+    task_page();
+    return res;
+}
--- src/idt/idt.h
+++ src/idt/idt.h
@@ -3,18 +3,39 @@
 
 #include <stdint.h>
 
-struct idt_desc {
-    uint16_t offset_low; // Offset bits 0-15
-    uint16_t selector; // Selector thats in out GDT
-    uint8_t zero;  // Does nothing
-    uint8_t type_attr; // Type and attributes
+struct idt_desc
+{
+    uint16_t offset_low;  // Offset bits 0-15
+    uint16_t selector;    // Selector thats in out GDT
+    uint8_t zero;         // Does nothing
+    uint8_t type_attr;    // Type and attributes
     uint16_t offset_high; // Offset bits 16-31
 } __attribute__((packed));
 
-
-struct idtr_desc {
+struct idtr_desc
+{
     uint16_t limit; // Size of descriptor table - 1
-    uint32_t base; // Base address of the interrupt descriptor table
+    uint32_t base;  // Base address of the interrupt descriptor table
+} __attribute__((packed));
+
+struct interrupt_frame
+{
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    /** This is stack pointer at the pushad moment
+     * It is not interesting for us because we have stack pointer below
+     * which was pushed by CPU on interrupt */
+    uint32_t reserved;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+    uint32_t ip;
+    uint32_t cs;
+    uint32_t flags;
+    uint32_t esp;
+    uint32_t ss;
 } __attribute__((packed));
 
 void idt_init();
