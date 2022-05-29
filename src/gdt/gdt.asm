877bd6c42b314102c7dce48e827dba8fcd68e273
Vasilii Rogin <vasya@rogin.ru>
2022-05-29 22:07:56 +0300

    Use first GDT entry to save GDT address and size

--- src/gdt/gdt.asm
+++ src/gdt/gdt.asm
@@ -2,6 +2,16 @@ section .asm
 global gdt_load
 
 gdt_load:
+    mov eax, [esp+4] ; GDT start address
+    mov [eax + 2], eax ; Set GDT address into first zero descriptor
+    push ebx
+    mov bx, [esp+8]
+    mov [eax], bx
+    pop ebx
+    lgdt [eax]
+    ret
+
+gdt_load_old:
     mov eax, [esp+4]
     mov [gdt_descriptor + 2], eax
     mov ax, [esp+8]
@@ -11,6 +21,6 @@ gdt_load:
 
 
 section .data
-gdt_descriptor:
+gdt_descriptor: ; Not used anymore
     dw 0x00 ; Size
     dd 0x00 ; GDT Start Address 
