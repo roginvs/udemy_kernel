365c3a724f440a1917911bc5e91bbdf521ce61ea
Vasilii Rogin <vasya@rogin.ru>
2022-05-30 18:31:01 +0300

    Add commit

--- src/keyboard/keyboard.c
+++ src/keyboard/keyboard.c
@@ -4,15 +4,14 @@
 #include "task/process.h"
 #include "task/task.h"
 
-static struct keyboard* keyboard_list_head = 0;
-static struct keyboard* keyboard_list_last = 0;
+static struct keyboard *keyboard_list_head = 0;
+static struct keyboard *keyboard_list_last = 0;
 
 void keyboard_init()
 {
-
 }
 
-int keyboard_insert(struct keyboard* keyboard)
+int keyboard_insert(struct keyboard *keyboard)
 {
     int res = 0;
     if (keyboard->init == 0)
@@ -25,33 +24,33 @@ int keyboard_insert(struct keyboard* keyboard)
     {
         keyboard_list_last->next = keyboard;
         keyboard_list_last = keyboard;
-    
     }
     else
     {
         keyboard_list_head = keyboard;
         keyboard_list_last = keyboard;
     }
-    
+
     res = keyboard->init();
 out:
     return res;
 }
 
-static int keyboard_get_tail_index(struct process* process)
+static int keyboard_get_tail_index(struct process *process)
 {
     return process->keyboard.tail % sizeof(process->keyboard.buffer);
 }
 
-void keyboard_backspace(struct process* process)
+void keyboard_backspace(struct process *process)
 {
-    process->keyboard.tail -=1 ;
+    // TODO: What if tail will go below head? Not very good
+    process->keyboard.tail -= 1;
     int real_index = keyboard_get_tail_index(process);
     process->keyboard.buffer[real_index] = 0x00;
 }
 void keyboard_push(char c)
 {
-    struct process* process = process_current();
+    struct process *process = process_current();
     if (!process)
     {
         return;
@@ -69,7 +68,7 @@ char keyboard_pop()
         return 0;
     }
 
-    struct process* process = task_current()->process;
+    struct process *process = task_current()->process;
     int real_index = process->keyboard.head % sizeof(process->keyboard.buffer);
     char c = process->keyboard.buffer[real_index];
     if (c == 0x00)
