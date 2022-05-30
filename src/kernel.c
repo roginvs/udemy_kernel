29c80b2c89420a4826419bce7f2e20a666ffbcd0
Vasilii Rogin <vasya@rogin.ru>
2022-05-30 23:01:50 +0300

    Remove old line

--- src/kernel.c
+++ src/kernel.c
@@ -149,8 +149,6 @@ void kernel_main()
     print_art();
     terminal_goto(0, 0);
 
-    idt_register_interrupt_callback(0x20, pic_timer_callback);
-
     struct process *process = 0;
     int res = process_load_switch("0:/blank.bin", &process);
     if (res != PEACHOS_ALL_OK)
