d29729132ce1d2fa32cc529565ce0fb17f64f903
Vasilii Rogin <vasya@rogin.ru>
2022-05-30 18:47:25 +0300

    Add comment

--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -12,19 +12,17 @@ static uint8_t keyboard_scan_set_one[] = {
     0x08, '\t', 'Q', 'W', 'E', 'R', 'T',
     'Y', 'U', 'I', 'O', 'P', '[', ']',
     0x0d, 0x00, 'A', 'S', 'D', 'F', 'G',
-    'H', 'J', 'K', 'L', ';', '\'', '`', 
+    'H', 'J', 'K', 'L', ';', '\'', '`',
     0x00, '\\', 'Z', 'X', 'C', 'V', 'B',
     'N', 'M', ',', '.', '/', 0x00, '*',
     0x00, 0x20, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, '7', '8', '9', '-', '4', '5',
-    '6', '+', '1', '2', '3', '0', '.'
-};
+    '6', '+', '1', '2', '3', '0', '.'};
 
 struct keyboard classic_keyboard = {
     .name = {"Classic"},
-    .init = classic_keyboard_init
-};
+    .init = classic_keyboard_init};
 
 int classic_keyboard_init()
 {
@@ -35,6 +33,7 @@ int classic_keyboard_init()
 uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
 {
     size_t size_of_keyboard_set_one = sizeof(keyboard_scan_set_one) / sizeof(uint8_t);
+    // TODO: >= here
     if (scancode > size_of_keyboard_set_one)
     {
         return 0;
@@ -44,13 +43,11 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
     return c;
 }
 
-
 void clasic_keyboard_handle_interrupt()
 {
-
 }
 
-struct keyboard* classic_init()
+struct keyboard *classic_init()
 {
     return &classic_keyboard;
 }
