57cb6f78880fa55d3060b99942845af62aae65f1
Vasilii Rogin <vasya@rogin.ru>
2022-06-06 21:38:46 +0300

    Add comment

--- programs/stdlib/src/peachos.asm
+++ programs/stdlib/src/peachos.asm
@@ -1,5 +1,7 @@
 [BITS 32]
 
+; TODO What this "function" do?
+; Improtant for dynamic linking? What is the difference if no ":function"?
 global print:function
 
 ; void print(const char* filename)
@@ -10,6 +12,6 @@ print:
     mov eax, 1 ; Command print
     int 0x80
     add esp, 4
-    
+
     pop ebp
     ret 
