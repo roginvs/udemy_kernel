a19f6cfaeee1ea06cf78395d7b10287985eb3dc8
Vasilii Rogin <vasya@rogin.ru>
2022-06-07 20:59:42 +0300

    Add Lecture 103

--- programs/stdlib/src/peachos.asm
+++ programs/stdlib/src/peachos.asm
@@ -1,10 +1,12 @@
 [BITS 32]
+section .asm
 
 ; TODO What this "function" do?
 ; Improtant for dynamic linking? What is the difference if no ":function"?
 global print:function
 global getkey:function
 global peachos_malloc:function
+global peachos_free:function
 
 ; void print(const char* filename)
 print:
@@ -36,3 +38,14 @@ peachos_malloc:
     add esp, 4
     pop ebp
     ret
+
+; void peachos_free(void* ptr)
+peachos_free:
+    push ebp
+    mov ebp, esp
+    mov eax, 5 ; Command 5 free (Frees the allocated memory for this process)
+    push dword[ebp+8] ; Variable "ptr"
+    int 0x80
+    add esp, 4
+    pop ebp
+    ret     
--- src/task/process.h
+++ src/task/process.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include "task.h"
 #include "config.h"
+#include <stdbool.h>
 
 // Process filetype used on loading stage
 #define PROCESS_FILETYPE_ELF 0
@@ -56,5 +57,6 @@ int process_load_for_slot(const char *filename, struct process **process, int pr
 struct process *process_current();
 struct process *process_get(int process_id);
 void *process_malloc(struct process *process, size_t size);
+void process_free(struct process *process, void *ptr);
 
 #endif
