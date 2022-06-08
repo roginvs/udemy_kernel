8411d58e46a7fbfd38151daec114701ebb78411e
Vasilii Rogin <vasya@rogin.ru>
2022-06-08 21:12:50 +0300

    Actually add Lecture 110'

--- programs/stdlib/src/peachos.asm
+++ programs/stdlib/src/peachos.asm
@@ -8,6 +8,7 @@ global peachos_getkey:function
 global peachos_malloc:function
 global peachos_free:function
 global peachos_putchar:function
+global peachos_process_load_start:function
 
 ; void print(const char* filename)
 print:
@@ -62,3 +63,14 @@ peachos_free:
     add esp, 4
     pop ebp
     ret     
+
+; void peachos_process_load_start(const char* filename)
+peachos_process_load_start:
+    push ebp
+    mov ebp, esp
+    mov eax, 6 ; Command 6 process load start ( stars a process )
+    push dword[ebp+8] ; Variable "filename"
+    int 0x80
+    add esp, 4
+    pop ebp
+    ret 
--- programs/stdlib/src/peachos.h
+++ programs/stdlib/src/peachos.h
@@ -13,4 +13,6 @@ void peachos_putchar(char c);
 int peachos_getkeyblock();
 void peachos_terminal_readline(char *out, int max, bool output_while_typing);
 
+void peachos_process_load_start(const char *filename);
+
 #endif
