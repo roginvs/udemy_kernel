8b3b3d9da553bd390ef54ab7555c3042b89e7c36
Vasilii Rogin <vasya@rogin.ru>
2022-06-15 20:04:27 +0300

    Add task switching

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -67,6 +67,14 @@ void idt_handle_exception()
     task_next();
 }
 
+void idt_clock()
+{
+    outb(0x20, 0x20);
+
+    // Switch to the next task
+    task_next();
+}
+
 void idt_init()
 {
     memset(idt_descriptors, 0, sizeof(idt_descriptors));
@@ -85,6 +93,8 @@ void idt_init()
     {
         idt_register_interrupt_callback(i, idt_handle_exception);
     }
+
+    idt_register_interrupt_callback(0x20, idt_clock);
 
     idt_load(&idtr_descriptor);
 }
